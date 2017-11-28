40608e239ec4e70397814f7df8589d1b93470e18
Jerry Lau <jerryzlau@gmail.com>
2017-11-28 10:32:41 -0800

    update server to take timeslot routes

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,18 @@ var mongoose = require('mongoose');
 var TimeSlot = require('./api/models/timeSlotModel');
 var bodyParser = require('body-parser');
 
+// mongoose url connection 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/TimeSlotdb');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+var routes = require('./api/routes/timeSlotRoutes');
+// register routes
+routes(app);
+
+// Listen on port 5000
 app.listen(port, () => {
   console.log('Listening on PORT:', port, '...');
 });
