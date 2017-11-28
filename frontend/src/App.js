74bb5900ed05860d45560c7c348771941db23763
Jerry Lau <jerryzlau@gmail.com>
2017-11-28 14:38:27 -0800

    created index action creator, make reducer next

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as APIs from './util/timeSlots_util';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  
+class App extends Component {  
   componentDidMount(){
-    APIs.deleteTimeSlot("5a1de247b0f7f77cf559b3fa").then(res =>
-      console.log(res)
-    );
+
   }
 
   render() {
-    console.log(this.state);
     return (
       <div className="App">
-        <h1>App</h1>
+        <h1>Time Sheet</h1>
       </div>
     );
   }
---
+++ frontend/src/reducers/timeSlot_reducer.js
@@ -0,0 +1,2 @@
+import { RECEIVE_TIMESLOTS } from '../actions/timeSlot_actions';
+import merge from 'loadash/merge';
