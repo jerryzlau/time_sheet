881d141f297107a0c9700e1afabddcef96d60cae
Jerry Lau <jerryzlau@gmail.com>
2017-12-01 20:12:50 -0800

    added another seeding method, one to visit /timeslotsSeed one does it when db is empty for demo purpose

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ var mongoose = require('mongoose');
 var TimeSlot = require('./api/models/timeSlotModel');
 var bodyParser = require('body-parser');
 var path = require('path');
+var seedTimeSheet = require('./api/seeds');
+var TimeSheet = mongoose.model("TimeSlots");
 
 // mongoose url connection 
 mongoose.Promise = global.Promise;
@@ -19,6 +21,17 @@ routes(app);
 
 app.use(function(req, res) {
   res.status(404).send({ url: req.originalUrl + " not found" });
+});
+
+// seeding the database 
+TimeSheet.find({}, (err, timeSlot) => {
+  if (err) {
+    console.log(err);
+  } else {
+    if(timeSlot.length === 0){
+      seedTimeSheet.seed_time_sheet();
+    }
+  }
 });
 
 // Listen on port 5000
